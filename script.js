5d9f518e6e7d7f56b44710c5b22a1e6e1bff8983
Arthur <arthuraugusto.exe@gmail.com>
2021-10-06 13:43:51 -0300

    improving script and css

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-let i = 0;
-
 function fotoSlide() {
-   
     let foto = document.getElementById('foto')
-    foto.style.animation = 'slide-left 0.5s both'
-    foto.style.WebkitAnimation = 'slide-left 0.5s both' 
+    let sobreMim = document.getElementById('sobreMim')
+    foto.style.animation = 'slide-left 1s both'
+    foto.style.WebkitAnimation = 'slide-left 1s both' 
     foto.setAttribute("onclick", "javascript: fotoReSlide();")
-    document.getElementById('sobreMim').display = 'inline-block'
+    sobreMim.style.display = 'block'
+    sobreMim.style.animation = 'slide-in-blurred-left 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both';
+    sobreMim.style.WebkitAnimation = 'slide-in-blurred-left 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both';
 }
 
-function fotoReSlide() {
-    
+function fotoReSlide() { 
     let foto = document.getElementById('foto')  
-    foto.style.animation = 'slide-right 0.5s both'
-    foto.style.WebkitAnimation ='slide-right 0.5s both'
+    let sobreMim = document.getElementById('sobreMim')
+    foto.style.animation = 'slide-right 1s both'
+    foto.style.WebkitAnimation ='slide-right 1s both'
     foto.setAttribute("onclick", "javascript: fotoSlide();")
-    document.getElementById('sobreMim').display = 'none'
+    sobreMim.style.display = 'none'
+    sobreMim.style.animation = 'fade-out 1s ease-out both';
+    sobreMim.style.WebkitAnimation = 'fade-out 1s ease-out both'; 
 }
+
+function mostraLista() {
+    let lista = document.querySelector('#listaJs')
+    if(lista.style.display == '') {
+        lista.style.display = 'block'
+    } else {
+        lista.style.display = ''
+    }
+}
+
+
